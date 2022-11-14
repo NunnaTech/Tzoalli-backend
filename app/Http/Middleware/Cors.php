b32642bb834c6cdd0ab39112c7ee3d0df0d8b774
Leonardo Cantoran <20183ti017@utez.edu.mx>
2022-11-13 22:07:15 -0600

    TEST
    TEST

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,6 +20,8 @@ class Cors
 
         ->header("Access-Control-Allow-Origin", "*")
         ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
-        ->header("Access-Control-Allow-Headers", "Content-Type, Authorization"); 
+        ->header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Request-With")
+        ->header("Access-Control-Allow-Credentials","true");
+  
     }
 }
