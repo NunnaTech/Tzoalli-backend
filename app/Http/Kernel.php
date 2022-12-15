99cd7c4ea8b16f5514f80c295c6e609ef36a188f
Raúl (Sterplay2010) <69400223+Sterplay2010@users.noreply.github.com>
2022-12-14 20:48:15 -0600

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,6 +39,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             //\App\Http\Middleware\Cors::class,
+            \Fruitcake\Cors\HandleCors::class,
         ],
 
         'api' => [
@@ -65,6 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        //'cors' => \App\Http\Middleware\Cors::class,
+        'cors' => \Fruitcake\Cors\HandleCors::class,,
     ];
 }
