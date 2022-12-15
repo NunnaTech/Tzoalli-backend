6a61f091ce1657fdab287920b4d562e272dff719
Raúl (Sterplay2010) <69400223+Sterplay2010@users.noreply.github.com>
2022-12-14 20:53:50 -0600

    Update kernel.php for cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        //\App\Http\Middleware\Cors::class,
+        \App\Http\Middleware\Cors::class,
     ];
 
     /**
@@ -38,8 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            //\App\Http\Middleware\Cors::class,
-            \Fruitcake\Cors\HandleCors::class,
+            \App\Http\Middleware\Cors::class,
         ],
 
         'api' => [
@@ -66,6 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        //'cors' => \Fruitcake\Cors\HandleCors::class,,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
